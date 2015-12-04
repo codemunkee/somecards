aa2eb6cd506837821b808c424b85ece869d8ff2e
Russ Nealis <rnealis@twilio.com>
2015-12-04 15:07:08 -0800

    Skeleton

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='somecards',
+    version='0.0.1',
+    packages=['somecards'],
+    url='',
+    license='',
+    author='Russ Nealis',
+    author_email='codemunkee@gmail.com',
+    description='API for somecards App',
+    install_requires=['Flask==0.10.1']
+)
---
+++ somecards/Card.py
@@ -0,0 +1,3 @@
+class IndexCard:
+    def __init__(self):
+        print 'Index Card!'
