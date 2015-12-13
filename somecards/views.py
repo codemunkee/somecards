837e67551d8db12159f6bbbd57c6fe45a0920794
Russ Nealis <rnealis@twilio.com>
2015-12-13 15:49:25 -0800

    Started adding logic to review cards of a specific category

--- somecards/views.py
+++ somecards/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, session, request, url_for
 from somecards import app, models, db
 from .forms import AddCardForm, RemoveCardForm
 
@@ -38,15 +38,32 @@ def review():
     return render_template('review.html', categories=categories)
 
 
-@app.route('/review/<int:category_id>')
+@app.route('/review/<int:category_id>', methods=['GET', 'POST'])
 def review_category(category_id):
-    cards = models.Card.query.filter_by(category_id=category_id)
-    return render_template('review.html', cards=cards)
+
+    # Get our cards for the specific category
+    cards = [(card.id, card.question, card.answer)
+             for card in models.Card.query.filter_by(category_id=category_id)]
+
+    # We have a dictionary to keep track of what card we were last on when
+    # we do reviews of a set of cards (a category). So, we make a dictionary
+    # here to hold this information if it doesn't already exist.
+    if not session.get('review'):
+        session['review'] = {}
+
+    # If there is not a sub dictionary for a specific category ID, we create it
+    # and set the index of the first card to 0.
+    if not session['review'].get(str(category_id)):
+        session['review'][str(category_id)] = {}
+        session['review'][str(category_id)]['current_index'] = 0
+
+    print session['review'][str(category_id)]['current_index']
+
+    return render_template('review.html', card=card)
 
 
 @app.route('/remove/<int:category_id>', methods=['GET', 'POST'])
 def remove_from_category(category_id):
-    # cards = [(card.id, card.question) for card in models.Card.query.all()]
     cards = [(card.id, card.question)
              for card in models.Card.query.filter_by(category_id=category_id)]
     form = RemoveCardForm()
@@ -66,7 +83,3 @@ def remove():
     categories = models.Category.query.all()
     return render_template('remove.html',
                            categories=categories)
-
-
-
-
